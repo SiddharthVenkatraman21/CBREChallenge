d5205cbea7747c90bba4a4f8c8dc12e7673fab46
ana-jimenez <anajimenez@tamu.edu>
2023-01-28 15:50:53 -0600

    updated algo 2

--- src/algover1.py
+++ src/algover1.py
@@ -1,36 +1,66 @@
 # don't touch this pls
 
-# teams will be a list of tuples
-# team = (<team number>:[<prefered teams>], [<tolerated teams>], [<no way teams>], <team size>)
-
-# floors will be a dictionary
-# floor = {<floor number>: <floor capacity>}
+# teams = ((<team number>, <number of members>, <list of preferred teams>, <list of tolerated teams>, <list of blacklisted teams>), ...)
+# floors = ((<floor number>, <capacity>), ...)
 
 def sort_teams(teams, floors):
-    # Sort teams based on the number of team members
-    teams = sorted(teams.items(), key=lambda x: x[1][0], reverse=True)
-    # Sort floors based on their capacity
-    floors = sorted(floors.items(), key=lambda x: x[1], reverse=True)
-    
-    # Place teams in the best available floor based on their preferences
+    # Create a dictionary to store the teams and their preferences
+    team_prefs = {}
     for team in teams:
-        placed = False
-        for floor in floors:
-            if placed:
-                break
-            if team[0] in floor[1][1]: # Check if team is in preferred teams list
-                floor[1][0].append(team[0])
-                placed = True
-            elif not any(t in floor[1][2] for t in team[1][2]) and not any(t in floor[1][3] for t in team[1][3]):
-                floor[1][0].append(team[0])
-                placed = True
-    # Check the capacity of each floor and make sure it is at least 25% full
+        team_num, team_size, preferred, tolerated, blacklisted = team
+        team_prefs[team_num] = {
+            'size': team_size,
+            'preferred': set(preferred),
+            'tolerated': set(tolerated),
+            'blacklisted': set(blacklisted)
+        }
+    # Create a list to store the final team assignments
+    assignments = []
+    # Sort the floors by capacity
+    floors = sorted(floors, key=lambda x: x[1])
+    # Iterate through each floor
     for floor in floors:
-        if len(floor[1][0])/floor[1] < 0.25:
-            for team in teams:
-                if len(floor[1][0])/floor[1] >= 0.25:
-                    break
-                if team[0] in floor[1][1]:
-                    floor[1][0].append(team[0])
-    # Return the final placement of teams in each floor
-    return {floor[0]: floor[1][0] for floor in floors}
+        floor_name, floor_capacity = floor
+        # Keep track of the remaining capacity for the current floor
+        remaining_capacity = floor_capacity
+        # Create a list to store the teams assigned to the current floor
+        assigned_teams = []
+        # Iterate through each team
+        for team_num, team_info in team_prefs.items():
+            # Check if the team has already been assigned
+            if team_num in [team[0] for team in assignments]:
+                continue
+            # Check if the team fits within the remaining capacity
+            if team_info['size'] > remaining_capacity:
+                continue
+            # Check if the team has any preferred teams assigned to the current floor
+            preferred_teams = [
+                team for team in assigned_teams if team[0] in team_info['preferred']
+            ]
+            if preferred_teams:
+                # Assign the team to the current floor
+                assigned_teams.append((team_num, team_info['size']))
+                remaining_capacity -= team_info['size']
+                continue
+            # Check if the team has any tolerated teams assigned to the current floor
+            tolerated_teams = [
+                team for team in assigned_teams if team[0] in team_info['tolerated']
+            ]
+            if tolerated_teams and remaining_capacity >= floor_capacity * 0.25:
+                # Assign the team to the current floor
+                assigned_teams.append((team_num, team_info['size']))
+                remaining_capacity -= team_info['size']
+                continue
+            # Check if the team has any blacklisted teams assigned to the current floor
+            blacklisted_teams = [
+                team for team in assigned_teams if team[0] in team_info['blacklisted']
+            ]
+            if not blacklisted_teams:
+                # Assign the team to the current floor
+                assigned_teams.append((team_num, team_info['size']))
+                remaining_capacity -= team_info['size']
+                continue
+        # Add the assigned teams to the final list of assignments
+        if assigned_teams:
+            assignments.append((floor_name, assigned_teams))
+    return assignments
